d0798ca71578e1eefd5309307c80a3e9cb84f271
Golam Sarwer <golamsarwer97@gmail.com>
2023-03-29 16:17:36 +0600

    initial commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstant {
+  static const String databaseId = '64240bc8c3ebd7de4876';
+  static const String projectId = '642402e43de6408ae7ec';
+  static const String endPoint = 'http://localhost/v1';
+}
